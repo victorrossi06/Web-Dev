a071cfb206899cd40dc54254a725e2f94f8c7ce9
Laboratório FIAP <labsfiap@fiap.com.br>
2026-03-24 20:44:59 -0300

    atualizando arquivos

--- script.js
+++ script.js
@@ -1,1 +1,119 @@
+/* let idade = 18
+let temCarteira = true
+
+if (idade >= 18 && temCarteira) {
+    console.log("Pode dirigir")
+}
+if (idade < 18 || !temCarteira) {
+    console.log("Não pode dirigir")
+} */
+
+/* let tarefaConcluida = true
+
+if (tarefaConcluida) {
+    console.log("A tarefa está concluida.")
+}
+let prioridade = 2 
+
+if (prioridade === 3) {
+    console.log("Prioridade alta")
+}
+else {
+    console.log("Prioridade não é alta.")
+}
+
+let prioridade2 = prioridade === 3 ? "Prioridade alta" : "Prioridade baixa"
+ */
+
+/* let prioridade = 2
+if (prioridade === 1) {
+    console.log("Prioridade baixa")
+}
+else if (prioridade === 2) {
+    console.log("Prioridade média")
+}
+else if (prioridade === 3) {
+    console.log("Prioridade alta")
+}
+else {
+    console.log("Prioridade desconhecida")
+} */
+
+let diaSemana = new Date().getDay()
+
+/* if (diaSemana === 0) {
+    console.log("Domingo")
+}
+else if (diaSemana === 1) {
+    console.log("Segunda")
+}
+else if (diaSemana === 0) {
+    console.log("Terça")
+}
+else if (diaSemana === 0) {
+    console.log("Quarta")
+}
+else if (diaSemana === 0) {
+    console.log("Quinta")
+}
+else if (diaSemana === 0) {
+    console.log("Sexta")
+}
+else if (diaSemana === 0) {
+    console.log("Sabado")
+} */
+
+/* switch (diaSemana) {
+    case 0:
+        console.log("Domingo")
+        break;
+    case 1:
+        console.log("Segunda")
+        break;
+    case 2:
+        console.log("Terça")
+        break;
+    case 3:
+        console.log("Quarta")
+        break;
+    case 4:
+        console.log("Quinta")
+        break;
+    case 5:
+        console.log("Sexta")
+        break;
+    case 6:
+        console.log("Sabado")
+        break;
+    default:
+        console.log("Dia invalido")
+        break;
+
+} */
+/* 
+for (let i = 0; i <= 5; i++) {
+    console.log("Contagem:", i)
+
+} */
+
+/* let contador = 0
+
+while (contador <= 5) {
+    console.log("Contagem: " + contador)
+    contador++
+} */
+
+let numero = 5
+
+do {
+    console.log("Numero é:", numero)
+    numero--
+} 
+while (numero > -98)
+
+
+
+
+
+
 
